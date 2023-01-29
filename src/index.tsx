b5daad0dc86f55f916506ff3fec9f8607e08f44a
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-01-29 20:28:41 +0300

    feat: add initial settings

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client"
+import "./common/styles/index.css"
+import App from "./components/app"
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(<App />)
