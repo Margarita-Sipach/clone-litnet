4c5cf67b32d9b1163d31dec8f6d98bf426bec731
Margarita <Margarita>
2023-02-03 19:22:42 +0300

    feat: add sidebar

---
+++ src/components/modules/sidebar/index.tsx
@@ -0,0 +1,18 @@
+import { ElementWrapper } from "../../ui/element-wrapper";
+import { SidebarElement } from "../../ui/sidebar-element";
+import { ReactNode } from "react";
+
+interface SidebarProps {
+  className?: string;
+	children: ReactNode
+}
+
+export const Sidebar = ({ className, children }: SidebarProps) => {
+  return (
+    <ElementWrapper
+      className={`hidden sm:flex sm:flex-col sm:w-80 sm:gap-y-6 lg:w-96 mr-5 ${className}`}
+    >
+			{children}
+    </ElementWrapper>
+  );
+};
