25621d8ff1bfcc33341f10631de2f9b57a109323
rin <prettysunshine468@gmail.com>
2023-02-07 00:50:01 +0600

    fix: add route for blog page

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -18,93 +18,98 @@ import AccountEdit from "../pages/Account/AccountEdit";
 import ContestPage from "../pages/ContestPage";
 import { RegistrationPage } from "../pages/registration";
 import { AuthorizationPage } from "../pages/authorization";
+import BlogPage from "../pages/BlogPage";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Root />,
-		errorElement: <NotFound />,
-		children: [
-			{
-				index: true,
-				element: <Home />,
-			},
-			{
-				path: "/blogs",
-				element: <Blogs />,
-			},
-			{
-				path: "/info",
-				element: <Info />,
-				children: [
-					{
-						path: "terms",
-						element: <Terms />,
-					},
-					{
-						path: "payment",
-						element: <Payment />,
-					},
-				],
-			},
-			{
-				path: "/registration",
-				element: <RegistrationPage />,
-			},
-			{
-				path: "/authorization",
-				element: <AuthorizationPage />,
-			},
-			{
-				path: "/contests",
-				element: <Contests />,
-			},
-			{
-				path: "/contests/:slug",
-				element: <ContestPage />,
-			},
-			{
-				path: "/book/:slug",
-				element: <BookPage />,
-			},
-			{
-				path: "/user/:slug",
-				element: <User />,
-				children: [
-					{
-						path: "about",
-						element: <UserAbout />,
-					},
-					{
-						path: "comments",
-						element: <UserComments />,
-					},
-					{
-						path: "following",
-						element: <UserFollowing />,
-					},
-				],
-			},
-			{
-				path: "/account",
-				element: <Account />,
-				children: [
-					{
-						index: true,
-						element: <AccountLibrary />,
-					},
-					{
-						path: "library",
-						element: <AccountLibrary />,
-					},
-					{
-						path: "edit",
-						element: <AccountEdit />,
-					},
-				],
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+      {
+        path: "/blogs/:id",
+        element: <BlogPage />,
+      },
+      {
+        path: "/info",
+        element: <Info />,
+        children: [
+          {
+            path: "terms",
+            element: <Terms />,
+          },
+          {
+            path: "payment",
+            element: <Payment />,
+          },
+        ],
+      },
+      {
+        path: "/registration",
+        element: <RegistrationPage />,
+      },
+      {
+        path: "/authorization",
+        element: <AuthorizationPage />,
+      },
+      {
+        path: "/contests",
+        element: <Contests />,
+      },
+      {
+        path: "/contests/:slug",
+        element: <ContestPage />,
+      },
+      {
+        path: "/book/:slug",
+        element: <BookPage />,
+      },
+      {
+        path: "/user/:slug",
+        element: <User />,
+        children: [
+          {
+            path: "about",
+            element: <UserAbout />,
+          },
+          {
+            path: "comments",
+            element: <UserComments />,
+          },
+          {
+            path: "following",
+            element: <UserFollowing />,
+          },
+        ],
+      },
+      {
+        path: "/account",
+        element: <Account />,
+        children: [
+          {
+            index: true,
+            element: <AccountLibrary />,
+          },
+          {
+            path: "library",
+            element: <AccountLibrary />,
+          },
+          {
+            path: "edit",
+            element: <AccountEdit />,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 function App() {
