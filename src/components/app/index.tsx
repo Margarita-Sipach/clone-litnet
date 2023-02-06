66bf179e3cba590d25f3111a382a6c9edbbfae68
Margarita <Margarita>
2023-02-06 16:12:52 +0300

    fix: fix router bag

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -18,95 +18,96 @@ import AccountEdit from "../pages/Account/AccountEdit";
 import ContestPage from "../pages/ContestPage";
 import { RegistrationPage } from "../pages/registration";
 import { AuthorizationPage } from "../pages/authorization";
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <NotFound />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/blogs",
-          element: <Blogs />,
-        },
-        {
-          path: "/info",
-          element: <Info />,
-          children: [
-            {
-              path: "terms",
-              element: <Terms />,
-            },
-            {
-              path: "payment",
-              element: <Payment />,
-            },
-          ],
-        },
-        {
-          path: "/registration",
-          element: <RegistrationPage />,
-        },
-        {
-          path: "/authorization",
-          element: <AuthorizationPage />,
-        },
-        {
-          path: "/contests",
-          element: <Contests />,
-        },
-        {
-          path: "/contests/:slug",
-          element: <ContestPage />,
-        },
-        {
-          path: "/book/:slug",
-          element: <BookPage />,
-        },
-        {
-          path: "/user/:slug",
-          element: <User />,
-          children: [
-            {
-              path: "about",
-              element: <UserAbout />,
-            },
-            {
-              path: "comments",
-              element: <UserComments />,
-            },
-            {
-              path: "following",
-              element: <UserFollowing />,
-            },
-          ],
-        },
-        {
-          path: "/account",
-          element: <Account />,
-          children: [
-            {
-              index: true,
-              element: <AccountLibrary />,
-            },
-            {
-              path: "library",
-              element: <AccountLibrary />,
-            },
-            {
-              path: "edit",
-              element: <AccountEdit />,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: <NotFound />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "/blogs",
+				element: <Blogs />,
+			},
+			{
+				path: "/info",
+				element: <Info />,
+				children: [
+					{
+						path: "terms",
+						element: <Terms />,
+					},
+					{
+						path: "payment",
+						element: <Payment />,
+					},
+				],
+			},
+			{
+				path: "/registration",
+				element: <RegistrationPage />,
+			},
+			{
+				path: "/authorization",
+				element: <AuthorizationPage />,
+			},
+			{
+				path: "/contests",
+				element: <Contests />,
+			},
+			{
+				path: "/contests/:slug",
+				element: <ContestPage />,
+			},
+			{
+				path: "/book/:slug",
+				element: <BookPage />,
+			},
+			{
+				path: "/user/:slug",
+				element: <User />,
+				children: [
+					{
+						path: "about",
+						element: <UserAbout />,
+					},
+					{
+						path: "comments",
+						element: <UserComments />,
+					},
+					{
+						path: "following",
+						element: <UserFollowing />,
+					},
+				],
+			},
+			{
+				path: "/account",
+				element: <Account />,
+				children: [
+					{
+						index: true,
+						element: <AccountLibrary />,
+					},
+					{
+						path: "library",
+						element: <AccountLibrary />,
+					},
+					{
+						path: "edit",
+						element: <AccountEdit />,
+					},
+				],
+			},
+		],
+	},
+]);
+
+function App() {
   return (
     <>
       <RouterProvider router={router} />
