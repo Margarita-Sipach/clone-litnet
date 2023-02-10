e8a6096fbd5c905c85a7e142bc108319e814c515
Margarita <Margarita>
2023-02-10 19:04:38 +0300

    refactor: remove comments

--- src/components/modules/registration-form/index.tsx
+++ src/components/modules/registration-form/index.tsx
@@ -1,14 +1,11 @@
 import { PrimaryButton } from "../../ui/primary-button";
 import { PrimaryInput } from "../../ui/primary-input";
-import { ChangeEvent, useState } from "react";
 
 import { FileInput } from "../../ui/file-input";
 
 interface registrationProps {}
 
 export const RegistrationForm = ({}: registrationProps) => {
-  // const [preview, setPreview] = useState(avatar);
-
   return (
     <form action="" className="items-left flex max-w-[500px] flex-col gap-y-5 ">
       <FileInput className="h-32 w-32" />
