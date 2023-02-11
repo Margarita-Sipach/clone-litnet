dad1d78307390729e836b1e5a6ccd3ccf066cc41
Margarita <Margarita>
2023-02-11 14:41:09 +0300

    feat: add textarea component

---
+++ src/components/ui/primary-textarea/index.tsx
@@ -0,0 +1,24 @@
+import { ChangeEvent } from "react";
+
+interface PrimaryTextareaProps {
+  attributes?: {
+    placeholder?: string;
+    required?: boolean;
+  };
+  onChange?: (e?: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export const PrimaryTextarea = ({
+  attributes,
+  onChange,
+}: PrimaryTextareaProps) => {
+  return (
+    <textarea
+      className="h-60 rounded-md border-[1px] bg-slate-100 py-2 px-4 text-slate-500 placeholder:text-slate-500 focus:border-indigo-400 focus:outline-none sm:text-sm sm:placeholder:text-sm lg:text-lg lg:placeholder:text-lg"
+      {...attributes}
+      onChange={(e?: ChangeEvent<HTMLTextAreaElement>) => {
+        if (onChange) onChange(e);
+      }}
+    />
+  );
+};
