87f99e88c7911ef9b8b93b4f19a78b0ef561eeda
rin <prettysunshine468@gmail.com>
2023-02-18 22:47:59 +0600

    feat: add onchange prop on primary-select component

--- src/components/ui/primary-select/index.tsx
+++ src/components/ui/primary-select/index.tsx
@@ -1,16 +1,23 @@
+import React, { ChangeEvent } from "react";
+
 interface PrimaryInputProps {
   title: string;
   options: (string | number)[];
   defaultOption?: string | number;
+  onChange?: (e?: ChangeEvent<HTMLSelectElement>) => void;
 }
 
 export const PrimarySelect = ({
   title,
   options,
   defaultOption,
+  onChange,
 }: PrimaryInputProps) => {
   return (
     <select
+      onChange={(e?: ChangeEvent<HTMLSelectElement>) => {
+        if (onChange) onChange(e);
+      }}
       defaultValue={defaultOption || title}
       className="w-full border-spacing-1 rounded border border-gray-400 py-2 px-3"
     >
