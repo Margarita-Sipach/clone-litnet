c3c75e5ecd12681f48b6cd08bd5f51cacf495771
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-04-04 00:59:04 +0300

    feat: add book info edit form

--- src/components/pages/account/book/AccountEditBookInfo/index.tsx
+++ src/components/pages/account/book/AccountEditBookInfo/index.tsx
@@ -1,5 +1,9 @@
 import { useParams } from "react-router-dom";
-import { useFetchBook, useFetchGenres } from "../../../../../hooks";
+import {
+  notifyError,
+  useFetchBook,
+  useFetchGenres,
+} from "../../../../../hooks";
 import useEditBook from "../../../../../hooks/account/useEditBook";
 import { useState, ChangeEvent, useEffect } from "react";
 import { Button } from "../../../../ui/buttons/Button";
@@ -10,91 +14,125 @@ import { PrimarySelect } from "../../../../ui/PrimarySelect";
 import { Textarea } from "../../../../ui/Textarea";
 import { Spinner } from "../../../../ui/Spinner";
 import { processImage } from "../../../../../utils/utils";
+import { useForm } from "react-hook-form";
+import {
+  ErrorInputMessages,
+  ErrorNotifies,
+  InputNames,
+} from "../../../../../utils/formUtils";
 
 export const AccountEditBookInfo = () => {
   const { bookId } = useParams();
   const { book } = useFetchBook(`${bookId}`);
   const { genres } = useFetchGenres();
-  const { editBook, isLoading } = useEditBook(`${bookId}`);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [firstSelectedGenre, setFirstSelectedGenre] = useState("");
-  const [secondSelectedGenre, setSecondSelectedGenre] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ mode: "onBlur" });
+  const { editBook, isLoading, isError, error } = useEditBook(`${bookId}`);
   const [file, setFile] = useState<File | null>(null);
-  const [defFile, setDefFile] = useState<string | null>(null);
-
-  useEffect(() => book && setTitle(book.title), [book]);
-  useEffect(() => book && setDefFile(book.img), [book]);
-  useEffect(
-    () =>
-      book &&
-      setFirstSelectedGenre(book.genres.length > 0 ? book.genres[0].name : ""),
-    [book]
-  );
-
-  useEffect(
-    () =>
-      book &&
-      setSecondSelectedGenre(book.genres.length > 1 ? book.genres[1].name : ""),
-    [book]
-  );
-
-  const createFormData = () => {
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("description", description);
-    formData.append("genres", `${firstSelectedGenre} ${secondSelectedGenre}`);
-    if (file) formData.append("img", file);
-    return formData;
-  };
 
   const handleSetFile = (e?: ChangeEvent<HTMLInputElement>) => {
     const files = (e?.target as HTMLInputElement)?.files;
     if (files) setFile(files[0]);
   };
 
-  const handleSubmitForm = () => {
-    editBook(createFormData());
+  const handleSubmitForm = (data) => {
+    editBook({ ...data, img: file });
   };
+
+  useEffect(() => {
+    if (isError && error) {
+      notifyError(ErrorNotifies.BOOK_EXISTS);
+    }
+  }, [error, isError]);
+
   return book && genres ? (
     <PageWrapper title="Редактировать информацию о книге">
       <div className="flex gap-x-5">
         <FileInput
           className="h-52 w-40"
           onChange={handleSetFile}
-          defaultImage={processImage(defFile)}
+          defaultImage={processImage(book.img)}
         />
         <div className="flex flex-grow flex-col gap-y-5">
           <Input
-            value={title}
+            value={book.title}
             placeholder="Название книги"
-            required={true}
-            onChange={(e) => setTitle(e?.target.value || "")}
+            properties={{
+              ...register(InputNames.TITLE, {
+                required: ErrorInputMessages.REQUIRED,
+                minLength: {
+                  value: 3,
+                  message: ErrorInputMessages.TITLE_LENGTH,
+                },
+                maxLength: {
+                  value: 24,
+                  message: ErrorInputMessages.TITLE_LENGTH,
+                },
+              }),
+            }}
+            name={InputNames.TITLE}
+            errors={errors}
           />
           <PrimarySelect
             title="Жанр 1"
             options={genres.map((g) => g.name)}
             defaultOption={book.genres.length > 0 ? book.genres[0].name : ""}
-            onChange={(e) => setFirstSelectedGenre(e.target.value || "")}
+            properties={{
+              ...register(InputNames.GENRE_FIRST, {
+                required: ErrorInputMessages.REQUIRED,
+                validate: (value, formData) =>
+                  value !== formData[InputNames.GENRE_SECOND]
+                    ? [...genres.map((g) => g.name)].includes(value)
+                      ? true
+                      : ErrorInputMessages.SELECT_GENRE
+                    : ErrorInputMessages.EQUALS_GENRES,
+              }),
+            }}
+            name={InputNames.GENRE_FIRST}
+            errors={errors}
           />
           <PrimarySelect
             title="Жанр 2"
             options={genres.map((g) => g.name)}
             defaultOption={book.genres.length > 1 ? book.genres[1].name : ""}
-            onChange={(e) => setSecondSelectedGenre(e.target.value || "")}
+            properties={{
+              ...register(InputNames.GENRE_SECOND, {
+                required: ErrorInputMessages.REQUIRED,
+                validate: (value, formData) =>
+                  value !== formData[InputNames.GENRE_FIRST]
+                    ? [...genres.map((g) => g.name)].includes(value)
+                      ? true
+                      : ErrorInputMessages.SELECT_GENRE
+                    : ErrorInputMessages.EQUALS_GENRES,
+              }),
+            }}
+            name={InputNames.GENRE_SECOND}
+            errors={errors}
           />
         </div>
       </div>
       <Textarea
         placeholder="Аннотация"
         required={true}
-        value={description}
-        onChange={(e) => setDescription(e?.target.value || "")}
+        value={book.description}
+        properties={{
+          ...register(InputNames.DESCRIPTION, {
+            maxLength: {
+              value: 400,
+              message: ErrorInputMessages.TEXT_LENGTH,
+            },
+          }),
+        }}
+        name={InputNames.DESCRIPTION}
+        errors={errors}
       />
       {isLoading ? (
         <Spinner className="flex w-full justify-center" />
       ) : (
-        <Button onClick={handleSubmitForm}>Сохранить</Button>
+        <Button onClick={handleSubmit(handleSubmitForm)}>Сохранить</Button>
       )}
     </PageWrapper>
   ) : (
--- src/hooks/account/useEditBook.ts
+++ src/hooks/account/useEditBook.ts
@@ -1,6 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
-import { updateBook } from "../../api/service";
+import { InputNames, createFormDataWithImage } from "../../utils/formUtils";
+import axios from "axios";
+import { baseUrl } from "../../utils/utils";
+
+const updateBook = async (id, data) => {
+  try {
+    const response = await axios.patch(`${baseUrl}/books/${id}`, data);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+const createCustomFormData = (data) => {
+  const formData = createFormDataWithImage(data);
+  formData.append(
+    InputNames.GENRE,
+    `${data[InputNames.GENRE_FIRST]} ${data[InputNames.GENRE_SECOND]}`
+  );
+  return formData;
+};
 
 const useEditBook = (id: string) => {
   const navigate = useNavigate();
@@ -8,15 +28,19 @@ const useEditBook = (id: string) => {
     mutate: editBook,
     isError,
     isLoading,
+    error,
   } = useMutation({
-    mutationFn: (data: any) => updateBook(`${id}`, data),
+    mutationFn: (data: any) => updateBook(`${id}`, createCustomFormData(data)),
     mutationKey: ["books", id],
     onSuccess: () => {
       navigate(-1);
     },
+    onError: (error) => {
+      throw error;
+    },
   });
 
-  return { editBook, isError, isLoading };
+  return { editBook, isError, isLoading, error };
 };
 
 export default useEditBook;
